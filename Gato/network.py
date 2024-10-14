b3d4bca8944d7ce64407040ff7db5b43cf494c7c
Meli Blancas <melifernandez@ciencias.unam.mx>
2024-10-13 18:55:17 -0600

    Gato

---
+++ Gato/network.py
@@ -0,0 +1,29 @@
+import socket
+import pickle
+
+class Network:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = "192.168.1.91"
+        self.port = 5555 
+        self.addr = (self.server, self.port)
+        self.p = self.connect()
+
+    def getP(self):
+        return self.p
+
+    def connect(self):
+        try:
+            self.client.connect(self.addr)
+            return self.client.recv(2048*3).decode() # Receive the id of the player
+        except:
+            pass
+
+    def send(self, data):
+        try:
+            self.client.send(str.encode(data)) # Send data to the server
+            return pickle.loads(self.client.recv(2048*2)) # Receive data from the server and decompose it to get the actual object
+        except socket.error as e:
+            print(e)
+
+
